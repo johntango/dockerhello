85cd2f30cdd3051b21a1b1a38d63349419bd1cf2
jrw0mitedu <jrw0mitedu>
2018-11-27 09:27:40 -0500

    go1

--- hello.js
+++ hello.js
@@ -1,6 +1,5 @@
 var express = require('express');
 var app     = express();
-var test = require('unit.js');
 
 app.get('/', function(request,response){
     response.send('Hello World!');
@@ -11,9 +10,3 @@ app.get('/time', function(request,response){
 });
 console.log("listening on 3000");
 app.listen(3000);
-// do a test 
-var str = 'Hello, world!';
-
-if (test.string(str).startsWith('Hello')) {
-  console.log('Passed');
-}
